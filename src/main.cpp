32104ce4cd54158070524d6eda4af90c22df7a3a
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-01-20 07:34:45 +0000

    moved 3 dots to the right making a line to get an idea of how it moves, then tried going down in an angle

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,20 @@
 #include <bn_color.h>
 #include <bn_core.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
 int main() {
     // Your code here
     bn::core::init();
     bn::backdrop::set_color(bn::color(20, 31, 20));
 
-
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(30, 10);
+     bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(50, 10);
+      bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(70, 10);
+       bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(70, 15);
+        bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(80, 25);
+         bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(90, 35);
+          bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(100, 45);
  while(true) 
     {
    
