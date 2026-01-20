e126e9472bc53cae228876fa86c140fe6587683b
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-01-20 07:56:01 +0000

    made 2 loops to print lines to my desired length and in my desired layout. shaping the letter L

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,31 @@
 #include <bn_keypad.h>
 #include <bn_sprite_ptr.h>
 #include <bn_sprite_items_dot.h>
+#include <bn_log.h>
+#include <bn_vector.h>
 int main() {
     // Your code here
     bn::core::init();
     bn::backdrop::set_color(bn::color(20, 31, 20));
+    bn::vector<bn::sprite_ptr, 10> circles = {};
+    bn::vector<bn::sprite_ptr, 20> circles2 = {};
+    for(int y = -40; y <= 40; y += 10)
+    {
+        bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-40, y);
+        circles2.push_back(myCircle2);
+    }
+ for(int x = -40; x <= 40; x += 10)
+    {
+        BN_LOG("x value", x);
+         bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(x, 40);
+         circles.push_back(myCircle);
 
-    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(30, 10);
-     bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(50, 10);
-      bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(70, 10);
-       bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(70, 15);
-        bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(80, 25);
-         bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(90, 35);
-          bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(100, 45);
+ }
+   
+    
  while(true) 
     {
+        
    
         if (bn::keypad::a_held() && bn::keypad::b_held()) 
                 {bn::backdrop::set_color(bn::color(20, 0, 20));} 
